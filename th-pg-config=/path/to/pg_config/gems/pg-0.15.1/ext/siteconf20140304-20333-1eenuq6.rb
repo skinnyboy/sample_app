ab3597873a9d8121de60436330a3779949060b82
GUILLEUX VINCENT <thecinematicorchestra@yahoo.fr>
2014-03-04 17:33:08 +0100

    Improve the README

---
+++ th-pg-config=/path/to/pg_config/gems/pg-0.15.1/ext/siteconf20140304-20333-1eenuq6.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140304-20333-7jrpgy"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
